7f57b1a4f2075eaa0aab9ec47abdd0e355c4685d
Jefferson Eduardo <jeff.eduard@gmail.com>
2020-10-08 17:52:06 -0300

    UP1

--- SistemaEstoque.Banco/tbLocalEstoque.cs
+++ SistemaEstoque.Banco/tbLocalEstoque.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data;
 using System.Data.SqlClient;
-using System.Dynamic;
 using System.Windows.Forms;
-
-
 
 namespace SistemaEstoque.Banco
 {
-    public class tbProdutos
+    public class tbLocalEstoque
     {
-        public int Id_Produto { get; set; }
-        public string Nome_Produto { get; set; }
-        public string Desc_Produto { get; set; }
-        public decimal Peso_Produto { get; set; }
+        public int Id_Local { get; set; }
+        public string Nome_Local { get; set; }
+
 
         public bool Inserir()
         {
@@ -23,19 +19,17 @@ namespace SistemaEstoque.Banco
             {
                 SqlCommand comando = new SqlCommand();
                 comando.Connection = Utilitarios.ConexaoBanco.conexao;
-                comando.CommandText = " INSERT INTO Produtos VALUES (@Nome, @Descricao, @Peso) ";
-                comando.Parameters.AddWithValue("@Nome", Nome_Produto);
-                comando.Parameters.AddWithValue("@Descricao", Desc_Produto);
-                comando.Parameters.AddWithValue("@Peso", Peso_Produto);
+                comando.CommandText = " INSERT INTO LocalEstoque VALUES (@Nome) ";
+                comando.Parameters.AddWithValue("@Nome", Nome_Local);
 
                 comando.ExecuteNonQuery();
 
                 return true;
 
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                MessageBox.Show("Erro ao inserir produto - Descrição : " + ex.Message.ToString());
+                MessageBox.Show("Erro ao inserir Local - Descrição : " + ex.Message.ToString());
                 return false;
             }
 
@@ -47,11 +41,10 @@ namespace SistemaEstoque.Banco
             {
                 SqlCommand comando = new SqlCommand();
                 comando.Connection = Utilitarios.ConexaoBanco.conexao;
-                comando.CommandText = " UPDATE Produtos SET Nome_Produto = @Nome, Desc_Produto = @Descricao, Peso_Produto = @Peso WHERE Id_Produto = @Id) ";
-                comando.Parameters.AddWithValue("@Id", Id_Produto);
-                comando.Parameters.AddWithValue("@Nome", Nome_Produto);
-                comando.Parameters.AddWithValue("@Descricao", Desc_Produto);
-                comando.Parameters.AddWithValue("@Peso", Peso_Produto);
+                comando.CommandText = " UPDATE LocalEstoque SET Nome_Local = @Nome WHERE Id_Local = @Id) ";
+                comando.Parameters.AddWithValue("@Id", Id_Local);
+                comando.Parameters.AddWithValue("@Nome", Nome_Local);
+                
 
 
                 comando.ExecuteNonQuery();
@@ -60,7 +53,7 @@ namespace SistemaEstoque.Banco
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao alterar produto - Descrição : " + ex.Message.ToString());
+                MessageBox.Show("Erro ao alterar Local - Descrição : " + ex.Message.ToString());
                 return false;
             }
         }
@@ -71,9 +64,9 @@ namespace SistemaEstoque.Banco
             {
                 SqlCommand comando = new SqlCommand();
                 comando.Connection = Utilitarios.ConexaoBanco.conexao;
-                comando.CommandText = " DELETE FROM Produtos WHERE Id_Produto = @Id) ";
-                comando.Parameters.AddWithValue("@Id", Id_Produto);
-                
+                comando.CommandText = " DELETE FROM LocalEstoque WHERE Id_Local = @Id) ";
+                comando.Parameters.AddWithValue("@Id", Id_Local);
+
 
                 comando.ExecuteNonQuery();
 
@@ -81,7 +74,7 @@ namespace SistemaEstoque.Banco
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao excluir produto - Descrição : " + ex.Message.ToString());
+                MessageBox.Show("Erro ao excluir Local - Descrição : " + ex.Message.ToString());
                 return false;
             }
         }
@@ -92,7 +85,7 @@ namespace SistemaEstoque.Banco
             {
                 SqlCommand comando = new SqlCommand();
                 comando.Connection = Utilitarios.ConexaoBanco.conexao;
-                comando.CommandText = " SELECT * FROM Produtos WHERE Id_Produto = @Id) ";
+                comando.CommandText = " SELECT * FROM LocalEstoque WHERE Id_Local = @Id) ";
 
 
                 DataTable dtRetorno = new DataTable();
@@ -102,9 +95,11 @@ namespace SistemaEstoque.Banco
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao Consultar produto - Descrição : " + ex.Message.ToString());
+                MessageBox.Show("Erro ao Consultar Local - Descrição : " + ex.Message.ToString());
                 return null;
             }
         }
     }
+
 }
+
